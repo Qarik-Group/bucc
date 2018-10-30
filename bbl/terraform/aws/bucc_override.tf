b775969f7df0ced13c438169f00535eada4d214d
ramonskie <ramonmakkelie@gmail.com>
2018-10-30 12:09:46 +0100

    removed upstream fixes and added vault port #160

--- bbl/terraform/aws/bucc_override.tf
+++ bbl/terraform/aws/bucc_override.tf
@@ -1,32 +1,3 @@
-# open port 8443 for uaa authentication
-resource "aws_security_group_rule" "concourse_lb_internal_8443" {
-  type        = "ingress"
-  protocol    = "tcp"
-  from_port   = 8443
-  to_port     = 8443
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = "${aws_security_group.concourse_lb_internal_security_group.id}"
-}
-
-resource "aws_lb_listener" "concourse_lb_8443" {
-  load_balancer_arn = "${aws_lb.concourse_lb.arn}"
-  protocol          = "TCP"
-  port              = 8443
-
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.concourse_lb_8443.arn}"
-  }
-}
-
-resource "aws_lb_target_group" "concourse_lb_8443" {
-  name     = "${var.short_env_id}-concourse8443"
-  port     = 8443
-  protocol = "TCP"
-  vpc_id   = "${local.vpc_id}"
-}
-
 # vars needed for bucc flags (which are included via common.sh)
 output "director__security_groups" {
   value = [
@@ -47,4 +18,14 @@ output "director__lb_target_groups" {
 
 output "director__concourse_domain" {
   value = "${aws_lb.concourse_lb.dns_name}"
+}
+
+# add vault port for jumpbox
+resource "aws_security_group_rule" "bosh_security_group_rule_vault" {
+  security_group_id        = "${aws_security_group.bosh_security_group.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 8200
+  to_port                  = 8200
+  source_security_group_id = "${aws_security_group.jumpbox.id}"
 }
--- bbl/terraform/gcp/bucc_override.tf
+++ bbl/terraform/gcp/bucc_override.tf
@@ -7,26 +7,6 @@ output "director__tags" {
   ]
 }
 
-resource "google_compute_firewall" "firewall-concourse" {
-  name    = "${var.env_id}-concourse-open"
-  network = "${google_compute_network.bbl-network.name}"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443", "2222" ,"8443"]
-  }
-
-  target_tags = ["concourse"]
-}
-
-resource "google_compute_forwarding_rule" "uaa-forwarding-rule" {
-  name        = "${var.env_id}-concourse-uaa"
-  target      = "${google_compute_target_pool.target-pool.self_link}"
-  port_range  = "8443"
-  ip_protocol = "TCP"
-  ip_address  = "${google_compute_address.concourse-address.address}"
-}
-
 # vars needed for bucc flags (which are included via common.sh)
 output "director__concourse_domain" {
   value = "${google_compute_address.concourse-address.address}"
@@ -34,4 +14,19 @@ output "director__concourse_domain" {
 
 output "director__target_pool" {
   value = "${google_compute_target_pool.target-pool.name}"
+}
+
+# add vault port for jumpbox
+resource "google_compute_firewall" "bosh-open" {
+  name    = "${var.env_id}-bosh-open"
+  network = "${google_compute_network.bbl-network.name}"
+
+  source_tags = ["${var.env_id}-bosh-open"]
+
+  allow {
+    ports    = ["22", "6868", "8443", "8844", "25555", "8200"]
+    protocol = "tcp"
+  }
+
+  target_tags = ["${var.env_id}-bosh-director"]
 }
